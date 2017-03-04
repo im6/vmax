9e4eb091ed652544004aee109be9a46140b83fb7
jinouwuque <ee2win@gmail.com>
2017-03-03 21:02:30 -0500

    remove redundancy

--- main.py
+++
@@ -1,9 +0,0 @@
-import tornado.ioloop
-from server.routes import routes
-
-settings = {
-    "cookie_secret": "cat and dog",
-    #"xsrf_cookies": True,
-}
-app = tornado.wsgi.WSGIApplication(routes, **settings)
-
